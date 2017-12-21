910463d591f92a17ca36582061553329b60ae57e
David Philipson <david.philipson@gmail.com>
2017-12-20 16:58:58 -0800

    Add space debris

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import bgLeftUrl = require("../static/bg_left.png");
 import bgRightUrl = require("../static/bg_right.png");
 import bgTopUrl = require("../static/bg_top.png");
 import imageUrl = require("../static/hank.png");
+import debrisRareUrl = require("../static/mars.jpg");
+import debrisCommonUrl = require("../static/moon.jpg");
+import smallPlanetUrl = require("../static/neptune.jpg");
 import planetUrl = require("../static/venus.jpg");
 import { Animation, runAnimations } from "./animationEngine";
 import * as Animations from "./animations";
@@ -23,9 +26,17 @@ const START_LOCATION = toRect({
 });
 const END_LOCATION = toRect({
     lat: Math.PI / 3,
-    lon: -3 * Math.PI / 3,
+    lon: -2 * Math.PI / 3,
     r: BIG_PLANET_RADIUS + 10,
 });
+const SMALL_PLANET_RADIUS = 32;
+const SMALL_PLANET_LOCATION = toRect({
+    lat: -Math.PI / 6,
+    lon: Math.PI,
+    r: BIG_PLANET_RADIUS + 40 + SMALL_PLANET_RADIUS,
+});
+const DEBRIS_COUNT = 16;
+
 const AMBIENT_LIGHTING_INTENSITY = 0.7;
 const POINT_LIGHT_COLOR = "#A66321";
 const POINT_LIGHT_INTENSITY = 6;
@@ -45,18 +56,9 @@ function main(): void {
     handleResizes(camera, renderer);
     addCanvas(renderer);
     addSkybox(scene);
-
-    const planetTexture = new THREE.TextureLoader().load(planetUrl);
-    const planetGeometry = new THREE.SphereBufferGeometry(
-        BIG_PLANET_RADIUS,
-        32,
-        32,
-    );
-    const planetMaterial = new THREE.MeshBasicMaterial({
-        map: planetTexture,
-    });
-    const planet = new THREE.Mesh(planetGeometry, planetMaterial);
-    scene.add(planet);
+    addBigPlanet(scene);
+    addSmallPlanet(scene);
+    addDebris(scene);
 
     const ambientLight = new THREE.AmbientLight();
     scene.add(ambientLight);
@@ -169,6 +171,49 @@ function addSkybox(scene: THREE.Scene): void {
     ]);
 }
 
+function addBigPlanet(scene: THREE.Scene): void {
+    const texture = new THREE.TextureLoader().load(planetUrl);
+    const geometry = new THREE.SphereBufferGeometry(BIG_PLANET_RADIUS, 32, 32);
+    const material = new THREE.MeshBasicMaterial({
+        map: texture,
+    });
+    const planet = new THREE.Mesh(geometry, material);
+    scene.add(planet);
+}
+
+function addSmallPlanet(scene: THREE.Scene): void {
+    const texture = new THREE.TextureLoader().load(smallPlanetUrl);
+    const geometry = new THREE.SphereBufferGeometry(
+        SMALL_PLANET_RADIUS,
+        16,
+        16,
+    );
+    const material = new THREE.MeshLambertMaterial({
+        map: texture,
+    });
+    const planet = new THREE.Mesh(geometry, material);
+    planet.position.copy(SMALL_PLANET_LOCATION);
+    scene.add(planet);
+}
+
+function addDebris(scene: THREE.Scene): void {
+    const textureCommon = new THREE.TextureLoader().load(debrisCommonUrl);
+    const textureRare = new THREE.TextureLoader().load(debrisRareUrl);
+    for (let i = 0; i < DEBRIS_COUNT; i++) {
+        const texture = Math.random() < 0.1 ? textureRare : textureCommon;
+        const lat = randomInRange(-Math.PI / 2, Math.PI / 2);
+        const lon = randomInRange(0, 2 * Math.PI);
+        const r = randomInRange(BIG_PLANET_RADIUS + 12, BIG_PLANET_RADIUS + 30);
+        const position = toRect({ lat, lon, r });
+        const radius = randomInRange(0.25, 2);
+        const geometry = new THREE.SphereBufferGeometry(radius, 16, 16);
+        const material = new THREE.MeshLambertMaterial({ map: texture });
+        const planet = new THREE.Mesh(geometry, material);
+        planet.position.copy(position);
+        scene.add(planet);
+    }
+}
+
 function handleResizes(
     camera: THREE.PerspectiveCamera,
     renderer: THREE.Renderer,
@@ -184,6 +229,10 @@ function addCanvas(renderer: THREE.Renderer): void {
     const canvas = renderer.domElement;
     canvas.style.position = "absolute";
     document.body.appendChild(canvas);
+}
+
+function randomInRange(min: number, max: number): number {
+    return min + (max - min) * Math.random();
 }
 
 main();
